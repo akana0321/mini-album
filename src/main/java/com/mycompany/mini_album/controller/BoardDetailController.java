db131f2e30a9c6532b952ac15dd3767191e49c65
akana0321 <akana0321@gmail.com>
2022-06-03 22:46:10 +0900

    Merge remote-tracking branch 'origin/boardlist'

--- src/main/java/com/mycompany/mini_album/controller/BoardDetailController.java
+++ src/main/java/com/mycompany/mini_album/controller/BoardDetailController.java
@@ -1,5 +1,42 @@
 package com.mycompany.mini_album.controller;
 
-public class BoardDetailController {
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.mini_album.dto.Board;
+import com.mycompany.mini_album.service.BoardService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RestController
+@RequestMapping("/board")
+public class BoardDetailController {
+  @Resource
+  private BoardService boardService;
+
+  @GetMapping("/{bno}")
+  public Board detail(@PathVariable int bno) {
+    log.info("실행");
+    log.info(bno);
+    Board dbBoard = boardService.getBoard(bno);
+    log.info(dbBoard);
+    return dbBoard;
+  }
+  
+  @DeleteMapping("/{bno}")
+  public Map<String, String> delete(@PathVariable int bno) {
+    boardService.deleteBoard(bno);
+    Map<String, String> map = new HashMap<>();
+    map.put("result", "success");
+    return map;
+  }
 }
