cf8ee2432b2f582eec94f77f164236decbfd7a6d
akana0321 <akana0321@gmail.com>
2022-06-03 14:07:22 +0900

    Add MultipartFile Field at Image DTO

--- src/main/java/com/mycompany/mini_album/dto/Images.java
+++ src/main/java/com/mycompany/mini_album/dto/Images.java
@@ -1,9 +1,13 @@
 package com.mycompany.mini_album.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
 public class Images {
+  private MultipartFile imgFile;
+  
   private int ino;
   private int bno;
   private String ioname;
